f4aaa002da083644b26c06a190b1a3d5581f4d55
R4Z121 <m.rizkyazizi@gmail.com>
2023-12-08 23:45:16 +0700

    add new feature "Add New Note"

--- src/components/body/AppForm.jsx
+++ src/components/body/AppForm.jsx
@@ -1,19 +1,28 @@
 import { FaCheck, FaRegBookmark, FaBookmark, FaTrash } from "react-icons/fa";
+import ButtonForm from "../modal/modal-form/ButtonForm";
 import { showFormattedDate } from "../../utils/data";
 
-export default function AppForm ({noteInfo, titleChangeHandler, bodyChangeHandler, editNoteHandler, toggleArchiveHandler, deleteModal, updateStatus}) {
+export default function AppForm ({noteInfo, titleChangeHandler, bodyChangeHandler, editNoteHandler, toggleArchiveHandler, deleteModal, updateStatus, addNoteHandler}) {
   return (
     <form className="flex flex-col p-2 gap-4">
       <div className="flex flex-col gap-2">
         <input type="text" id="noteTitle" className="p-2 text-base sm:text-xl md:text-2xl lg:text-3xl outline-none bg-transparent border-b-2 border-black font-bold" value={noteInfo.title} onChange={titleChangeHandler} placeholder="Note Title" />
-        <p className="text-sm md:text-base pl-2">{showFormattedDate(noteInfo.createdAt)}</p>
+        {noteInfo.createdAt ? (<p className="text-sm md:text-base pl-2">{showFormattedDate(noteInfo.createdAt)}</p>) : (<></>)}
       </div>
       <textarea id="noteBody" className="bg-white border-none outline-none p-2 text-sm sm:text-base md:text-lg lg:text-xl resize-none h-72" value={noteInfo.body} onChange={bodyChangeHandler} placeholder="Note Body"></textarea>
-      <div className="flex bg-app-blue w-full max-w-xl self-center gap-8 justify-center rounded text-xl sm:text-2xl md:text-3xl p-4 mt-16 md:mt-10">
-        <button type="button" title="simpan" className={updateStatus ? "text-green-400" : "text-zinc-500"} onClick={editNoteHandler}><FaCheck /></button>
-        <button type="button" title="arsip" className="text-black" onClick={toggleArchiveHandler}>{noteInfo.archived ? (<FaBookmark />) : (<FaRegBookmark />)}</button>
-        <button type="button" title="hapus" className="text-red-800" onClick={deleteModal}><FaTrash /></button>
-      </div>
+      {editNoteHandler ? (
+        <div className="flex bg-app-blue w-full max-w-xl self-center gap-8 justify-center rounded text-xl sm:text-2xl md:text-3xl p-4 mt-16 md:mt-10">
+          <button type="button" title="simpan" className={updateStatus ? "text-green-400" : "text-zinc-500"} onClick={editNoteHandler}><FaCheck /></button>
+          <button type="button" title="arsip" className="text-black" onClick={toggleArchiveHandler}>{noteInfo.archived ? (<FaBookmark />) : (<FaRegBookmark />)}</button>
+          <button type="button" title="hapus" className="text-red-800" onClick={deleteModal}><FaTrash /></button>
+        </div>
+      ) : (<></>) }
+      {addNoteHandler ? (
+        <div className="flex p-4 justify-end w-full">
+          <ButtonForm actionHandler={addNoteHandler} content="Simpan Catatan" type="button" customClass="bg-app-blue" />
+        </div>
+      ) : (<></>)}
+      
     </form>
   )
 }
--- src/pages/NewNotes.jsx
+++ src/pages/NewNotes.jsx
@@ -1,3 +1,31 @@
+import PageHeader from "../components/header/PageHeader";
+import AppForm from "../components/body/AppForm";
+import { addNote } from "../utils/data";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function NewNotes () {
-  return <h1>Ini halaman tambah catatan</h1>
+  const navigate = useNavigate();
+  const [noteInfo, setNoteInfo] = useState({title: '', body: ''});
+
+  const onTitleChange = e => {
+    setNoteInfo({...noteInfo, title: e.target.value});
+  }
+
+  const onBodyChange = e => {
+    setNoteInfo({...noteInfo, body: e.target.value});
+  }
+
+  //add new note handler
+  const addNewNote = () => {
+    addNote(noteInfo);
+    navigate('/');
+  }
+
+  return (
+    <div className="flex flex-col relative" id="add">
+      <PageHeader />
+      <AppForm noteInfo={noteInfo} titleChangeHandler={onTitleChange} bodyChangeHandler={onBodyChange} addNoteHandler={addNewNote} />
+    </div>
+  )
 }
