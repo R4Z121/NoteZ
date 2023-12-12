0408117e6a5d1fdd4521a058c2612d64eb41c73d
R4Z121 <m.rizkyazizi@gmail.com>
2023-12-12 23:33:04 +0700

    Fitur login (50%)

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { createContext, useState } from "react";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Archived from "./pages/Archived";
 import Detail from "./pages/Detail";
 import NewNotes from "./pages/NewNotes";
 import NotFound from "./pages/NotFound";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+export const AppContext = createContext(null);
 
 export default function App () {
+  const [authedUser, setAuthedUser] = useState(null);
+
+  const contextValue = {
+    authedUser,
+    setAuthedUser
+  }
+
   return (
-    <div className="flex flex-col min-h-screen justify-between gap-7 bg-thick-white">
-      <div className="flex flex-col relative">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/archived" element={<Archived />} />
-          <Route path="/detail/:noteId" element={<Detail />} />
-          <Route path="/new" element={<NewNotes />} />
-          <Route path="/:any" element={<NotFound />} />
-        </Routes>
+    <AppContext.Provider value={contextValue}>
+      <div className="flex flex-col min-h-screen justify-between gap-7 bg-thick-white">
+        <div className="flex flex-col relative">
+          <Routes>
+            <Route path="/login" element={authedUser ? (<Navigate to="/" />) : (<Login />)} />
+            <Route path="/register" element={authedUser ? (<Navigate to="/" />) : (<Register />)} />
+            <Route path="/" element={authedUser ? (<Home />) : (<Navigate to="/login" />)} />
+            <Route path="/archived" element={authedUser ? (<Archived />) : (<Navigate to="/login" />)} />
+            <Route path="/detail/:noteId" element={authedUser ? (<Detail />) : (<Navigate to="/login" />)} />
+            <Route path="/new" element={authedUser ? (<NewNotes />) : (<Navigate to="/login" />)} />
+            <Route path="/:any" element={<NotFound />} />
+          </Routes>
+        </div>
+        <footer className="flex justify-center items-center p-2">
+          <h4 className="text-base font-bold">Powered by R4Z121.</h4>
+        </footer>
       </div>
-      <footer className="flex justify-center items-center p-2">
-        <h4 className="text-base font-bold">Powered by R4Z121.</h4>
-      </footer>
-    </div>
+    </AppContext.Provider>
   )
 }
---
+++ src/pages/Login.jsx
@@ -0,0 +1,84 @@
+import { FaUser } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
+import ButtonForm from "../components/modal/modal-form/ButtonForm";
+import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { login } from "../utils/dataSource";
+import { AppContext } from "../App";
+
+export default function Login () {
+
+  const {setAuthedUser} = useContext(AppContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailInvalid, setEmailInvalid] = useState(false);
+  const [passwordInvalid, setPasswordInvalid] = useState(false);
+  
+  const onEmailInput = e => {
+    setEmail(e.target.value);
+    setEmailInvalid(false);
+  }
+
+  const onPasswordInput = e => {
+    setPassword(e.target.value);
+    setPasswordInvalid(false);
+  }
+
+  //handle login
+  const onLogin = async () => {
+    if(validateInput(email,password)) {
+      const {error,data} = await login({email,password});
+      if(!error) {
+        setAuthedUser(data);
+      } else {
+        console.log(error);
+      }
+    }
+  }
+
+  //validate email and password handler
+  const validateInput = (email,password) => {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+      if (password.length >= 6) {
+        return true;
+      } else {
+        setPasswordInvalid(true);
+        return false;
+      }
+    }
+    setEmailInvalid(true);
+    return false;
+  }
+
+  return (
+    <div className="p-3 w-full min-h-screen flex justify-center items-center">
+      <form className="w-full max-w-md flex flex-col gap-4 p-4 bg-app-blue text-white rounded">
+        <div id="form-header" className="w-full self-center relative -top-10 bg-blue-300 p-3 flex justify-center items-center font-bold text-lg sm:text-2xl border-4 border-blue-700">
+          <h1 className="text-blue-700">NoteZ</h1>
+        </div>
+        <div id="form-input" className="-mt-4 flex flex-col gap-5">
+          <div id="email-input" className="">
+            <div className={`flex items-center gap-2 bg-white p-2 text-black rounded-sm text-sm sm:text-lg ${emailInvalid ? "border-4 border-red-600" : ""}`}>
+              <FaUser />
+              <input type="email" id="email" className="p-1 w-full outline-none border-none bg-transparent" placeholder="Email" value={email} onChange={onEmailInput} required />
+            </div>
+            {emailInvalid ? (<p className="text-red-600 font-bold">Invalid email</p>) : (<></>)}
+          </div>
+          <div id="password-input">
+            <div className={`flex items-center gap-2 bg-white p-2 text-black rounded-sm text-sm sm:text-lg ${passwordInvalid ? "border-4 border-red-600" : ""}`}>
+              <FaLock />
+              <input type="password" id="password" className="p-1 w-full outline-none border-none bg-transparent" placeholder="Password" value={password} onChange={onPasswordInput} required />
+            </div>
+            {passwordInvalid ? (<p className="text-red-600 font-bold">Password must have atleast 6 characters length</p>) : (<></>)}
+          </div>
+        </div>
+        <ButtonForm content="Masuk" type="button" actionHandler={onLogin} customClass="w-full bg-blue-400 text-sm sm:text-lg hover:bg-blue-500" />
+        <div className="flex flex-col gap-3 text-center">
+          <hr />
+          <p>Belum punya akun ?</p>
+          <Link to="/register" className="p-2 text-white outline-0 border-none rounded hover:cursor-pointer w-full bg-blue-700 text-sm sm:text-lg hover:bg-blue-600">Daftar</Link>
+        </div>
+      </form>
+    </div>
+  )
+}
